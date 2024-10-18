37357f2072d3bb23d5a74af68539a36c62de5515
jlestel <jlestel@gmail.com>
2024-10-18 23:45:26 +0200

    fix: battery_states are unused

--- bigquery.sql
+++ bigquery.sql
@@ -149,7 +149,7 @@ FROM `MY_PROJECT.MY_VIN.charging_states` c
 LEFT JOIN `MY_PROJECT.MY_VIN.vehicle_states` v ON FORMAT_TIMESTAMP('%Y-%m-%d %H:%M:00', v.Timestamp__UTC_)= FORMAT_TIMESTAMP('%Y-%m-%d %H:%M:00', c.Timestamp__UTC_)
 LEFT JOIN `MY_PROJECT.MY_VIN.climate_states` clim ON FORMAT_TIMESTAMP('%Y-%m-%d %H:%M:00', clim.Timestamp__UTC_)= FORMAT_TIMESTAMP('%Y-%m-%d %H:%M:00', c.Timestamp__UTC_)
 LEFT JOIN `MY_PROJECT.MY_VIN.driving_states` d ON FORMAT_TIMESTAMP('%Y-%m-%d %H:%M:00', d.Timestamp__UTC_)= FORMAT_TIMESTAMP('%Y-%m-%d %H:%M:00', c.Timestamp__UTC_)
-LEFT JOIN `MY_PROJECT.MY_VIN.battery_states` b ON FORMAT_TIMESTAMP('%Y-%m-%d %H:%M:00', b.Timestamp__UTC_)= FORMAT_TIMESTAMP('%Y-%m-%d %H:%M:00', c.Timestamp__UTC_)
+/*LEFT JOIN `MY_PROJECT.MY_VIN.battery_states` b ON FORMAT_TIMESTAMP('%Y-%m-%d %H:%M:00', b.Timestamp__UTC_)= FORMAT_TIMESTAMP('%Y-%m-%d %H:%M:00', c.Timestamp__UTC_)*/
 WHERE FORMAT_TIMESTAMP('%Y-%m', c.Timestamp__UTC_) = 'MY_MONTH'
 GROUP BY c.Timestamp__UTC_, FORMAT_TIMESTAMP('%Y-%m-%d %H:%M:00', c.Timestamp__UTC_)
 order by FORMAT_TIMESTAMP('%Y-%m-%d %H:%M:00', c.Timestamp__UTC_)
